81140e3396454a683f3acb13239747602b33d6a1
CJ Batausa <caiusjayce.jaye@gmail.com>
2022-10-19 14:27:59 +0800

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+    return ( 
+        <div className="dashboard">
+            <h1>My MDC, My Family</h1>
+        </div>
+     );
+}
+ 
+export default Dashboard;
